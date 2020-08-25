5d8e7a48322339db89ccb3a6ee5d8b0d956b0d98
V Anastassiou <v.anastassiou@gmail.com>
2020-08-25 14:21:42 -0700

    Set up second app, include urlpattern

--- meaningless-folder-name/vsite/urls.py
+++ meaningless-folder-name/vsite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')), # References a different app's urlpatterns
+    path('admin/', admin.site.urls), # Always use include(); this is the one exception
 ]
