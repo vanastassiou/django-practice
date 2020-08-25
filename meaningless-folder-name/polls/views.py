25ee11ab81738ac4f7fb629844792f35a2f70575
V Anastassiou <v.anastassiou@gmail.com>
2020-08-25 16:13:52 -0700

    Implement poll detail view and template

--- meaningless-folder-name/polls/views.py
+++ meaningless-folder-name/polls/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    context = {'latest_question_list': latest_question_list} # context is a dict mapping template var names to Python objects (key and value must be named the same)
+    return render(request, 'polls/index.html', context) # can sub in dict instead of defining a var containing a dict
 
 def detail(request, question_id):
-    return HttpResponse("This is question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raiseHttp404("The requested question does not exist. Sorry about that.")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse("These are the results for question %s" % question_id)
