2100467f8a51fb4ece0a336d7c419427e2160c94
V Anastassiou <v.anastassiou@gmail.com>
2020-08-25 15:51:48 -0700

    Argh, forgot the gitignore

--- meaningless-folder-name/polls/views.py
+++ meaningless-folder-name/polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("This is the polls app's index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("This is question %s" % question_id)
