6ecffeda37cd85f41dd199138b031f78ba026a5e
V Anastassiou <v.anastassiou@gmail.com>
2020-08-25 16:31:41 -0700

    Refactor 404 syntax

--- meaningless-folder-name/polls/views.py
+++ meaningless-folder-name/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
@@ -11,10 +11,7 @@ def index(request):
     return render(request, 'polls/index.html', context) # can sub in dict instead of defining a var containing a dict
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raiseHttp404("The requested question does not exist. Sorry about that.")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
